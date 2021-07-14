0298d0790ff172ee4db78a2c844cc5ead738c3a9
Vasyl <dudka.vasia2010@gmail.com>
2021-07-14 20:46:32 +0300

    Inmplemented localStorage store saving

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,16 +4,25 @@ import { RouterModule } from '@angular/router'
 
 import { MatToolbarModule } from '@angular/material/toolbar'
 
-import { StoreModule } from '@ngrx/store'
+import { ActionReducer, StoreModule } from '@ngrx/store'
 import { StoreDevtoolsModule } from '@ngrx/store-devtools'
 import { StoreRouterConnectingModule } from '@ngrx/router-store'
+import { localStorageSync } from 'ngrx-store-localstorage'
 
 import { MaterialModule } from '../shared/material/material.module'
 
 import { NavbarComponent } from './components/navbar/navbar.component'
 import { NavbarUiComponent } from './components/navbar/navbar-ui.component'
 
+import { homeFeatureKey } from '../home/store'
+
 import { coreReducers } from './store'
+import { uiFeatureKey } from './store/ui'
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function localStorageSyncReducer(reducer: ActionReducer<any>): ActionReducer<any> {
+  return localStorageSync({ keys: [homeFeatureKey, uiFeatureKey], rehydrate: true })(reducer)
+}
 
 @NgModule({
   declarations: [
@@ -23,7 +32,7 @@ import { coreReducers } from './store'
   imports: [
     CommonModule,
     RouterModule,
-    StoreModule.forRoot(coreReducers),
+    StoreModule.forRoot(coreReducers, { metaReducers: [localStorageSyncReducer] }),
     StoreRouterConnectingModule.forRoot(),
     StoreDevtoolsModule.instrument(),
     MaterialModule,
